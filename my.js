cbc6174c64df4e565a4d8e17c54efb18f014ebe8
vishill <92530538+vishill@users.noreply.github.com>
2022-03-24 21:04:59 +0530

    Add files via upload

---
+++ my.js
@@ -0,0 +1,13 @@
+
+function testGS(){
+	
+	const url = "https://script.google.com/macros/s/AKfycbwZsKP8Q955AdqBD9M04qjNAwiSnC4SZuiP4wNGZtl8u4wJdgjHCl3IXPh5uW6t-qHz/exec";
+	
+	fetch(url)
+		.then(d => d.json())
+		.then(d => {
+			document.getElementById("app").textContent = d[0].status;
+		});
+}
+
+document.getElementById("btn").addEventListener("click",testGS);
